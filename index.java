39f8b1b529317a9a3ff4d20e3149da0deea75dab
Prajwal66 <74700045+Prajwal66@users.noreply.github.com>
2022-02-01 11:44:40 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-class test{
-public static void main(String args[]){
-for(int i = 1; i<=10;i++)
-{
-}[D
-[A[C[C[A
-[D[A[C 
-[DSystem.out.println("Hiii");
-}
-}
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
 }
