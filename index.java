38df90a7c34703cba307ad77a466d68e8733fe81
prajwalsuryawanshi <prajwalsuryawanshi12@gmail.com>
2022-02-01 11:41:01 +0530

    added codes files

---
+++ index.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class test{
+public static void main(String args[]){
+for(int i = 1; i<=10;i++)
+{
+}[D
+[A[C[C[A
+[D[A[C 
+[DSystem.out.println("Hiii");
+}
+}
+}
