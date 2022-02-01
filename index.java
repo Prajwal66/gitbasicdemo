c1950f8ba30b7e7eb1d80e2a99b7648e867364cd
Prajwal66 <74700045+Prajwal66@users.noreply.github.com>
2022-02-01 12:47:35 +0530

    Update index.java

--- index.java
+++ index.java
@@ -6,5 +6,7 @@ public class MyFirstJavaProgram {
 
    public static void main(String []args) {
       System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World"); // prints Hello World
+
    }
 }
